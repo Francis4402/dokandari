7c4e7f58059573545b4ae686df584b26247c5002
Francis4402 <francism4402@gmail.com>
2026-08-17 01:05:14 +0600

    config updated

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', 'https://www.haatpoint.com'),
 
-    'asset_url' => env('ASSET_URL', 'https://www.haatpoint.com'),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
